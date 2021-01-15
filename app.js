46cac20c0a19d464057592a48dcbedd59b9efe79
Sebastian Ward <sebastian.ward1@ibm.com>
2021-01-15 09:40:01 -0800

    Socket emit on arrow keystroke

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-let keypress = require ('keypress');
-let io       = require ('socket.io-client');
+const keypress = require ('keypress');
+const io       = require ('socket.io-client');
 
 
-const socket = io ('10.0.0.22');
+const socket = io ('http://localhost:3000'); // replace with arduino server ip
 
 keypress (process.stdin);
 
 process.stdin.on ('keypress', (ch, key) =>
 {
+
+    if (['left', 'right', 'up', 'down'].includes (key.name))
+    {
+       socket.emit ('eventName', key.name);
+    }
+
     if (key && key.ctrl && key.name == 'c') 
     {
-   
-        if (['left, right', 'up', 'down'].includes (key.name))
-        {
-            
-        }
-        process.stdin.pause ();
+        process.exit (1);
     }
 })
 
 
 
 process.stdin.setRawMode (true);
-process.stdin.resume();
